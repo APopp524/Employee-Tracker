ff12967ed15416799054ad017443e6b4e411e040
Andrew Popp <apopp524@gmail.com>
2020-09-02 11:10:37 -0500

    update seeds to given example

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Employee-Tracker
+
+### Directions
+Clone the GitHub repo.
+
+Run `npm install` to install all dependencies. To use the application locally, run `node server.js` in your CLI. And run `schema.sql` in MySQLWorkbench.
+
+### Description
+Developers are often tasked with creating interfaces that make it easy for non-developers to view and interact with information stored in databases. Often these interfaces are known as Content Management Systems. In this homework assignment, your challenge is to architect and build a solution for managing a company's employees using node, inquirer, and MySQL.
+
+### Technologies That Were Used
+MySQL | Express.js | Node.js | JavaScript | JQuery
+
+### Demo
+
+
+
+
--- seeds.sql
+++ seeds.sql
@@ -6,28 +6,28 @@ VALUES
     ('Sales'),
     ('Engineering'),
     ('Finance'),
-    ('Marketing');
+    ('Legal');
 
 INSERT INTO role
     (title, salary, department_id)
 VALUES
-    ('Sales Manager', 100000, 1),
+    ('Sales Lead', 100000, 1),
     ('Salesperson', 80000, 1),
-    ('Full-Stack Developer', 90000, 2),
+    ('Lead Engineer', 150000, 2),
     ('Software Engineer', 120000, 2),
-    ('Account Manager', 140000, 3),
-    ('Accountant', 110000, 3),
-    ('Marketing Coordinator', 100000, 4),
-    ('Director of Marketing', 180000, 4);
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
 
 INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
 VALUES
-    ('Andrew', 'Popp', 1, NULL),
-    ('Mikey', 'John', 2, 1),
-    ('Susan', 'Banana', 3, NULL),
-    ('Joey', 'Joseph', 4, 3),
-    ('Mike', 'Jordan', 5, NULL),
-    ('Kobe', 'Bryant', 6, 5),
-    ('Lionel', 'Messi', 7, NULL),
-    ('Kelsey', 'Allen', 8, 7);
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
